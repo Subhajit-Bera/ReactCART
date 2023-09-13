2013b05c0ebe83f8e6ce8dc36c71da149625331b
Subhajit-Bera <103336107+Subhajit-Bera@users.noreply.github.com>
2023-09-13 22:31:40 +0530

    route&header

---
+++ reactcart/src/App.js
@@ -0,0 +1,18 @@
+import {BrowserRouter,Routes,Route} from "react-router-dom";
+import Home from "./components/Home";
+import Header from "./components/Header";
+
+import "./styles/app.scss";
+
+function App() {
+  return (
+    <BrowserRouter>
+    <Header/>
+    <Routes>
+      <Route path="/" element={<Home/>}/>
+    </Routes>
+    </BrowserRouter>
+  );
+}
+
+export default App;
